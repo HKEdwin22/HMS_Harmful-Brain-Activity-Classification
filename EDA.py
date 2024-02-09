5fc5c01ff3fc839fbb7222cf4197431f6e6b8e38
KaverH <spongyworm@gmail.com>
2024-02-09 13:45:15 +0000

    Imported module from other paths

--- EDA.py
+++ EDA.py
@@ -1,1 +1,22 @@
 # Import libraries
+import sys
+sys.path.append('C:\\Users\\spong\\Documents\\DSAI\\ConvScripts')
+                
+import ForBeginning as fb
+import os
+import pandas as pd
+import dtale as dt
+
+# Functions and classes
+
+
+# Main program
+if __name__ == '__main__':
+    
+    dir_mydoc = fb.ChangeDir()
+
+    # Overview of train.csv
+    file = './train.csv'
+    df = fb.DescriptiveStat(file, 'csv')
+
+pass
