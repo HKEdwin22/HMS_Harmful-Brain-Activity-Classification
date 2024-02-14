61955070c1b62823d6ad1b2f1a9ea146c69b0d05
KaverH <spongyworm@gmail.com>
2024-02-14 15:34:38 +0000

    Creating balanced training set

--- EDA.py
+++ EDA.py
@@ -1,10 +1,9 @@
 # Import libraries
-import sys
+import sys, os
 sys.path.append('C:\\Users\\spong\\Documents\\DSAI\\ConvScripts')
                 
 import ForBeginning as fb
 import modules as md
-import os
 import pandas as pd
 import polars as pl
 import dtale as dt
@@ -17,8 +16,8 @@ from datetime import datetime
 # Functions and classes
 class Config():
     usrIn = False
-    rawPath = './rawData'
-    augPath = './augData'
+    rawPath = './rawData/'
+    augPath = './augData/'
 
 # Main program
 if __name__ == '__main__':
@@ -27,15 +26,44 @@ if __name__ == '__main__':
     start = time.time()
     dir_mydoc = fb.ChangeDir()
     
+    # Descriptive statistics for number of EEG samples for each EEG entry
+    file = Config.augPath + 'EEG_sampleNumber.csv'
+    df = fb.DescriptiveStat(file, file.split('.')[-1])
 
+    # Visualise analysis
+    df = df[(df['sample_num'] <= 10) & (df['sample_num'] >= 2)]
+    df = df.groupby(['sample_num', 'patient_id'])['eeg_id'].count().to_frame(name='eeg_entry_count')
+    df.reset_index(inplace=True)
+
+    for snum in df.sample_num:
+        dfTmp = df[df.sample_num == snum]
+        print('\n' + '='*20 + f'For sample number equals to {snum}')
+        print('\n' + '='*20 + ' Number of unique values ' + '='*20)
+        print(dfTmp.nunique())
+        print('\n' + '='*20 + ' Descriptive statistics for each column ' + '='*20 + '\n')
+        print(dfTmp.describe())
+    
     if Config.usrIn == True:
 
         # Overview of train.csv
         df = fb.DescriptiveStat(file, file.split('.')[-1])
 
         # Check if EEG records match with EEG parquet
-        df = md.CheckEEG('./train.csv', './train_eegs.zip')
+        eda = md.EDA()
+        df = eda.SampleRecMatch('./train.csv', './train_eegs.zip')
         df.to_csv('train_matchedEEG.csv', index=False)
+
+        # Count EEG sample number for each EEG id & EEG record number for each subject
+        eda = md.EDA()
+        eda.SamplingSize(Config.rawPath + 'train.csv')
+
+        # Descriptive statistics for number of EEG samples for each EEG entry
+        file = Config.augPath + 'EEG_sampleNumber.csv'
+        df = fb.DescriptiveStat(file, file.split('.')[-1], True)
+
+        # Descriptive statistics for number of EEG entry for each subject
+        file = Config.augPath + 'patient_eegNumber.csv'
+        df = fb.DescriptiveStat(file, file.split('.')[-1], True)
 
     end = time.time()
 
